2ac7cf8c871dfecbd6e8aa1f9be137a466e7e9ba
jekovcar <jekovcar@gmail.com>
2025-04-06 17:09:30 +0300

    Update BatchUnpack.bat

--- BatchUnpack.bat
+++ BatchUnpack.bat
@@ -5,6 +5,7 @@ move %~dp0utils\Issfix_iconextr.exe "%~dp0"
  %~dp0Issfix_iconextr.exe
 move %~dp0install_script.iss "%~dp0/%%f_unpack"
 move %~dp0SetupIcon.ico "%~dp0/%%f_unpack"
+xcopy /isvy %~dp0\Languages "%~dp0/%%f_unpack\embedded"
 move %~dp0Issfix_iconextr.exe "%~dp0\utils"
 move %~dp0/%%f_unpack "%~dp0/%%f_unpacked"
 )
