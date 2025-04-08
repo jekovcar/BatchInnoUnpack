36eb848bd2ae2522141b7e7942ee6ec890c604b9
jekovcar <jekovcar@gmail.com>
2025-04-08 12:57:29 +0300

    Update BatchUnpack_with_Log.bat

--- BatchUnpack_with_Log.bat
+++ BatchUnpack_with_Log.bat
@@ -1,17 +1,22 @@
+@Echo Off
+
 if not exist "%~dp0utils/innounp.exe" goto :message2
 if not exist "%~dp0*.exe" goto :message
 
 @echo.
-@echo --- Unpacking EXE files with Log --- 
+@echo --- Unpacking EXE files with Log ---
+@echo. 
 BatchUnpack.bat >%~dp0Unpack_Log.txt
 exit
 
 :message 
 @echo. 
 @echo === EXE files not exist ===
+@echo.
 pause
 
 :message2 
 @echo. 
 @echo === innounp.exe miss in utils ===
+@echo.
 pause
