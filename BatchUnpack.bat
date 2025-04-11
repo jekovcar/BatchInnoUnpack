23200060eac7830c802bbe670f767006a416169d
jekovcar <jekovcar@gmail.com>
2025-04-11 09:12:34 +0300

    Update BatchUnpack.bat

--- BatchUnpack.bat
+++ BatchUnpack.bat
@@ -4,6 +4,7 @@ if not exist "%~dp0utils/innounp.exe" goto :message2
 if not exist "%~dp0*.exe" goto :message
 
 for %%f in (*.exe) do ( 
+rmdir /S /Q %~dp0%%f_unpacked
  %~dp0utils/innounp.exe -x -m -a -d%~dp0/%%f_unpack %~dp0\%%~nxf
 move %~dp0%%f_unpack\install_script.iss "%~dp0"
 move %~dp0utils\Issfix_iconextr.exe "%~dp0"
@@ -12,7 +13,7 @@ move %~dp0install_script.iss "%~dp0/%%f_unpack"
 move %~dp0SetupIcon.ico "%~dp0/%%f_unpack"
 xcopy /isvy %~dp0\Languages "%~dp0/%%f_unpack\embedded"
 move %~dp0Issfix_iconextr.exe "%~dp0\utils"
-move %~dp0/%%f_unpack "%~dp0/%%f_unpacked"
+move %~dp0%%f_unpack "%~dp0%%f_unpacked"
 )
 exit
 
