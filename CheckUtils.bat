0da93a2be3ce12efe9ca2f6721ab72062d9159d5
jekovcar <jekovcar@gmail.com>
2025-05-06 06:56:03 +0300

    Update CheckUtils.bat

--- CheckUtils.bat
+++ CheckUtils.bat
@@ -21,62 +21,35 @@ powershell -NoLogo -NoProfile -Command "(Get-Item -Path '%~dp0utils/disasm.exe')
 
 :start
 @echo.
-@echo.Check the latest "Unicode version of the console application Inno Setup Unpacker" online?
+@echo.Check online Update or Overwrite the latest "Unicode version of the console application Inno Setup Unpacker" ?
 SET choice=
-SET /p choice=Pls, enter Y[N]: 
+SET /p choice=Pls, enter Y/N/O: 
 IF NOT '%choice%'=='' SET choice=%choice:~0,1%
 IF /i '%choice%'=='Y' GOTO yes
 IF /i '%choice%'=='N' GOTO no
+IF /i '%choice%'=='O' GOTO over
 ECHO "%choice%" is not valid
 ECHO.
 GOTO start
 
 :no
+@echo.
 ECHO It will close!
 PAUSE
 EXIT
 
 :yes
 rundll32 url.dll,FileProtocolHandler https://www.rathlev-home.de/tools/prog-e.html#unpack
- 
-:update
-ECHO.
-@echo.Update to the latest "Unicode version of the console application Inno Setup Unpacker"?
-SET choice=
-SET /p choice=Pls, enter U[L]: 
-IF NOT '%choice%'=='' SET choice=%choice:~0,1%
-IF /i '%choice%'=='U' GOTO upd
-IF /i '%choice%'=='L' GOTO lve
-ECHO "%choice%" is not valid
-ECHO.
-GOTO update
-
-:lve
-ECHO It will close!
-PAUSE
-EXIT
-
-:upd
-@echo.
-@echo Please wait for download https://www.rathlev-home.de/tools/download/innounp-2.zip
-@echo and unpack it to /utils
-powershell -command "Start-BitsTransfer -Source https://www.rathlev-home.de/tools/download/innounp-2.zip -Destination innounp-2.zip"
-powershell -command "Expand-Archive innounp-2.zip utils -Force"
-@echo.
-@echo Downloaded and unpacked "innounp-2.zip" in utils
-DEL innounp-2.zip /S /Q
-@echo.
-@echo. --- Now innounp.exe is updated ---
-@echo.--- Close to Exit [ OR ] ---^>^>
-pause
-goto :check
+GOTO over
 
 :message2
 color 06 
 @echo. 
 @echo === innounp.exe miss in utils ===
 @echo.
-@echo.For download (~600kb) and install
+:over
+@echo.
+@echo.For download (~600kb) latest innounpacker and install
 pause
 @echo Please wait for download https://www.rathlev-home.de/tools/download/innounp-2.zip
 @echo and unpack it to /utils
