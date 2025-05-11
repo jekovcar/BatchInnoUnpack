681833898bbc5efc8c8288cceba3f36358cf4dec
jekovcar <jekovcar@gmail.com>
2025-05-11 15:21:33 +0300

    Update BatchUnpack.bat

--- BatchUnpack.bat
+++ BatchUnpack.bat
@@ -45,7 +45,10 @@ color 06
 @echo. 
 @echo === innounp.exe miss in utils ===
 @echo.
-@echo.For download (~600kb) and install
+powershell "exit $PSVersionTable.PSVersion.Major"
+if %errorlevel% GEQ 5 (
+@Echo Off
+@echo.For download ~600kb and install
 pause
 @echo Please wait for download https://github.com/jrathlev/InnoUnpacker-Windows-GUI/raw/refs/heads/master/innounp-2/bin/innounp-2.zip
 @echo and unpack it to /utils
@@ -54,10 +57,13 @@ powershell -command "Expand-Archive innounp-2.zip utils -Force"
 @echo.
 @echo Downloaded and unpacked "innounp-2.zip" in utils
 DEL innounp-2.zip /S /Q
-@echo.
-@echo.--- Now innounp.exe exist in utils ---
-@echo.--- Close to Exit [ OR ] ---^>^>
+) else (
+color fc
+@echo.Download and install are not support by current powershell version:
+    powershell -command "(Get-Host).Version.ToString()"
+@echo.Download ~600kb it manually from  https://github.com/jrathlev/InnoUnpacker-Windows-GUI/raw/refs/heads/master/innounp-2/bin/innounp-2.zip
 pause
+)
 goto :check
 
 :message3
@@ -65,10 +71,20 @@ color 0e
 @echo. 
 @echo === wtee.exe miss in utils === (wtee writes logfile) 
 @echo.
-@echo.For download (~60kb) and install
+powershell "exit $PSVersionTable.PSVersion.Major"
+if %errorlevel% GEQ 5 (
+@Echo Off
+@echo.For download ~60kb and install
 pause
 @echo Please wait for download https://github.com/WinLAFS/wintee/releases/download/v1.0.1/wtee.exe
 @echo and move to /utils
 powershell -command "Start-BitsTransfer -Source https://github.com/WinLAFS/wintee/releases/download/v1.0.1/wtee.exe"
 powershell -command "Move-Item -Path wtee.exe -Destination utils"
+) else (
+color fc
+@echo.Download and install are not support by current powershell version:
+    powershell -command "(Get-Host).Version.ToString()"
+@echo.Download ~60kb it manually from https://github.com/WinLAFS/wintee/releases/download/v1.0.1/wtee.exe
+pause
+)
 goto :check
